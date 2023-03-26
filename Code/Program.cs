d8ceb3995073b72e7b0f7672368845758c59bb30
Elena Sofiyskaya <lena.sofiyskaya@mail.ru>
2023-03-26 23:57:45 +0300

    added the solution for task

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// method searches the strings in the array, that are equal to or less than 3 symbols
+void SymArr(string [] FirstArr, string [] FinalArr, int count)
+{
+    int result = 0;
+    for(int i = 0; i < FirstArr.Length; i++)
+    {
+        if(FirstArr[i].Length <= count)
+        {
+            FinalArr[result] = FirstArr[i];
+            result++;
+        }
+    }
+}
+
+// method prints 1D array into console
+void Print1DArr(string[] arr)
+{
+    Console.Write("[");
+
+    for(int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1]);
+    Console.WriteLine("]");
+}
+
+string [] FirstArr = new string[5] {"Hi", "456", "element", ":)", "FinalWork"};
+string [] FinalArr = new string[FirstArr.Length];
+
+SymArr(FirstArr, FinalArr, 3);
+Print1DArr(FinalArr);
